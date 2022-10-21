3bf3d30a8fd1e8c2afe40e58cd00520b5e65146e
onetrue5200 <oneruexie@gmail.com>
2022-10-21 13:03:28 +0800

    init gin server & set PORT=3000 in .env

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"go-jwt/initializers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+}
 
 func main() {
-	fmt.Println("Hello world")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 }
