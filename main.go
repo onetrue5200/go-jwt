9fcd3d2ddb8556d711dec97ab04b2f289eb9a89f
onetrue5200 <oneruexie@gmail.com>
2022-10-21 14:42:40 +0800

    add signup api

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"go-jwt/controllers"
 	"go-jwt/initializers"
 
 	"github.com/gin-gonic/gin"
@@ -14,10 +15,6 @@ func init() {
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.POST("/signup", controllers.Signup)
 	r.Run()
 }
